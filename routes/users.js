369e150bfa7a3b56644477bcdb3809363fc719da
Samurais <hailiang.hl.wang@gmail.com>
2016-06-11 17:52:03 +0800

    enable http://localhost:3000/users/rank

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../service/user');
+var config = require('../config/environment');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/rank', function(req, res, next) {
+
+    user.getRank(config.githubIds)
+        .then(function(data) {
+            res.json({
+                rc: 2,
+                data: data
+            });
+        })
+        .fail(function(err) {
+            res.json({
+                rc: 1,
+                error: err
+            });
+        });
 });
 
 module.exports = router;
