de973e5b732cd23a2b41867f28c15112590c6a81
Samurais <hailiang.hl.wang@gmail.com>
2016-06-13 16:14:32 +0800

    config/env/development.sample.js

---
+++ src/config/env/development.sample.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Expose
+ */
+
+module.exports = {
+    db: 'mongodb://localhost/noobjs_dev',
+    github: {
+        clientID: process.env.GITHUB_CLIENTID,
+        clientSecret: process.env.GITHUB_SECRET,
+        callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    fccusers: ['samurais',
+        'cuifan'
+    ]
+};
